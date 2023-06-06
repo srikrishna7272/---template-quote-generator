2c460178ff8eb4d87e86fc8b10986900e802bc21
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-06-06 16:53:32 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+let apiQuotes = [];
+// Show new quote
+function newQuote() {
+    // pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //Check if author field is blank and replace it with 'Unknown'
+    if(!quote.author) {
+        authorText.textContent = 'Unknown'
+    }else{
+        authorText.textContent = quote.author;
+
+    }
+    //Check Quote length to determine styling
+    if(quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+        
+    }catch (error) {
+        // Catch Error Here
+    }
+}
+// Tweet Quote
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+// On Load
+getQuotes();
